61d0260089c2c877d4005ea2d72778a4bc7b0c0b
David Ali Zambrano Burgos <davazamb10@outlook.com>
2017-07-15 10:20:39 -0400

    Proyecto copar mover y eliminar.  System.IO

---
+++ CopiarMoverEliminarFile/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace CopiarMoverEliminarFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCopiar = new System.Windows.Forms.Button();
+            this.btnMover = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCopiar
+            // 
+            this.btnCopiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCopiar.Location = new System.Drawing.Point(88, 107);
+            this.btnCopiar.Name = "btnCopiar";
+            this.btnCopiar.Size = new System.Drawing.Size(75, 23);
+            this.btnCopiar.TabIndex = 0;
+            this.btnCopiar.Text = "Copiar";
+            this.btnCopiar.UseVisualStyleBackColor = true;
+            this.btnCopiar.Click += new System.EventHandler(this.btnCopiar_Click);
+            // 
+            // btnMover
+            // 
+            this.btnMover.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMover.Location = new System.Drawing.Point(169, 107);
+            this.btnMover.Name = "btnMover";
+            this.btnMover.Size = new System.Drawing.Size(75, 23);
+            this.btnMover.TabIndex = 1;
+            this.btnMover.Text = "Mover";
+            this.btnMover.UseVisualStyleBackColor = true;
+            this.btnMover.Click += new System.EventHandler(this.btnMover_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Location = new System.Drawing.Point(250, 107);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 325);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnMover);
+            this.Controls.Add(this.btnCopiar);
+            this.Name = "Form1";
+            this.Text = "Copiar Mover y Eliminar Archivos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCopiar;
+        private System.Windows.Forms.Button btnMover;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ CopiarMoverEliminarFile/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace CopiarMoverEliminarFile
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            btnCopiar.Click += new EventHandler(btnCopiar_Click);
+            btnMover.Click += new EventHandler(btnMover_Click);
+            btnEliminar.Click += new EventHandler(btnEliminar_Click);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCopiar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofdc = new OpenFileDialog();
+            ofdc.ShowDialog();
+            string sourcepath = ofdc.FileName;
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+
+            string destpath = fbd.SelectedPath;
+
+            try
+            {
+                System.IO.File.Copy(sourcepath, destpath + "!!" + ofdc.SafeFileName);
+                MessageBox.Show("Archivo copiado exitosamente.");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void btnMover_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofdm = new OpenFileDialog();
+            ofdm.ShowDialog();
+            string sourcepath = ofdm.FileName;
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+
+            string destpath = fbd.SelectedPath;
+
+            try
+            {
+                System.IO.File.Move(sourcepath, destpath + "!!" + ofdm.SafeFileName);
+                MessageBox.Show("Archivo Movido exitosamente.");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            string filename = ofd.FileName;
+
+            try
+            {
+                System.IO.File.Delete(filename);
+                MessageBox.Show("Seleccione archivo :" + filename + "Eliminado exitosamente.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());  
+            }
+        }
+    }
+}
